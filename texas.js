1da0443b8f30b3b999798b0ac0b3eb885d829058
madsray <30870145+madsray@users.noreply.github.com>
2019-03-19 20:13:22 -0500

    Arlington Contact pop up working

---
+++ texas.js
@@ -0,0 +1,17 @@
+console.log("HELLO");
+function popArlington() {
+  var x = document.getElementById("contactA");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function popHouston() {
+  var x = document.getElementById("contactB");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
